b99cd3563388ed3f019337524ddcd646b9cf444f
Andrew Philpott and neha doodi <andrew@>
2020-02-05 08:41:58 -0800

    Add test to see if vitality, intelligence, and strength are persisted to the character object.

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -4,9 +4,24 @@ describe('Character', () => {
 
   test('Should be able to make a character object', () => {
     let vitality = 5;
-    let strength = 5;
     let intelligence = 10;
+    let strength = 5;
     let character = new Character(vitality, intelligence, strength);
     expect(character instanceof Character).toEqual(true);
+  });
+
+  test('Should be able to make a persist the vitality, intelligence, and strength values to a Character object', () => {
+    let vitality = 5;
+    let intelligence = 10;
+    let strength = 5;
+    
+    let character = new Character(2,2,2);
+    let vitalityFromCharacterObject = character.vitality;
+    let intelligenceFromCharacterOject = character.intelligence;
+    let strengthFromCharacterObject = character.strength;
+
+    expect(vitality).toEqual(vitalityFromCharacterObject);
+    expect(intelligence).toEqual(intelligenceFromCharacterOject);
+    expect(strength).toEqual(strengthFromCharacterObject);
   });
 });
