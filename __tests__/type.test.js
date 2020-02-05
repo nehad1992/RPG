67c25d449607cfd67d3e66d914efc606cc300f18
Andrew Philpott and neha doodi <andrew@>
2020-02-05 14:33:25 -0800

    pass test for character determination

--- __tests__/type.test.js
+++ __tests__/type.test.js
@@ -4,6 +4,6 @@ describe('Type', () => {
     test('should be able to increase the vitality, strength, intelligence based on character', () => {
         let typeOfCharacter = 'warrior' ; 
         let type = new Type(typeOfCharacter);
-        expect(type.name).toEqual("wizard");
+        expect(type.name).toEqual(typeOfCharacter)
     })
 });
